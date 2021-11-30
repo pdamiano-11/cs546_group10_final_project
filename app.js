381a5ae48abfd1a049e3d6f11690c3bf4c691c48
ethan_foskey23 <efoskey@stevens.edu>
2021-11-30 16:07:40 -0500

    Basic routes for create and update memories with handlebars

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const configRoutes = require('./routes');
+const { engine } = require('express-handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', __dirname + '/views');
+app.engine('handlebars', engine({ extname: '.handlebars', defaultLayout: "main"}));
+app.set('view engine', 'handlebars');
 
 configRoutes(app);
 
