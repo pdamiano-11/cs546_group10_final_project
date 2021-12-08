886d881077b3d71f36a8ddf59522c718fe9665f3
unknown <direstok@gmail.com>
2021-12-08 01:54:03 -0500

    more client side form vaidation

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,17 @@
-$(function() {
-    $('#login-form').submit(function(event) {
+$(function () {
+    const username = $('#username').val().trim();
+    const password = $('#password').val().trim();
+
+    $('#login-form').submit(function (event) {
         event.preventDefault();
-       const username = $('#username').val().trim()
-       const password = $('#password').val().trim()
+        
         if (username && password) {
             $('#error').hide();
             $('#login-form').trigger('reset');
         } else {
-           $('#error').show();
-           $('#username').focus(); 			
+            $('#error').show();
+            $('#username').focus();
         }
-   });
+    });
 });
+
