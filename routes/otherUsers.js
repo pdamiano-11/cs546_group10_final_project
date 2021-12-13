e00e9e661b2ba8a921805832d956dde08cdc18a4
unknown <direstok@gmail.com>
2021-12-12 23:03:30 -0500

    no profile picture

--- routes/otherUsers.js
+++ routes/otherUsers.js
@@ -24,29 +24,12 @@ router.post('/', async (req, res) => {
         const other_user = await usersData.getUserByUsername(other_username);
         console.log(user);
         return res.render('memory/profileFound',{
-            profilePicture: other_user.profilePicture,
             firstName: other_user.firstName,
             email: other_user.email,
             gender: other_user.gender,
             age: other_user.age,
             username: other_user.username
     });
-    } catch (e) {
-        res.render('memory/profileSearch',{error: 'Unable to find user'})
-    }
-});
-
-router.get('/gallery', async (req, res) => {
-    if(!req.session.user){
-        res.redirect('/login');
-    }
-    try{
-        const user = req.session.user;
-        const other_username = await req.body;
-        console.log(other_username);
-        const other_user = await getUserByUsername(other_username);
-        console.log(user);
-        return res.json(other_user.memories);
     } catch (e) {
         res.render('memory/profileSearch',{error: 'Unable to find user'})
     }
